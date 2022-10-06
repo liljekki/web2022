dd8c9eea95a954f4c9d183d1c85edf9bed1bbc64
Kvartiuk <alexkvmit11@gmail.com>
2022-10-06 05:48:00 +0300

    edit box

--- labs/lab3/js/main.js
+++ labs/lab3/js/main.js
@@ -23,4 +23,60 @@ window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
 
- 
+
+/* let cart = {
+    'name': {
+        "name": "viper",
+        count: 2,
+        price: 200
+    },
+    'gg': {
+        "name": "viper_mini",
+        count: 2,
+        price: 300
+    }
+};
+document.onclick = event => {
+    
+} */
+
+$(document).ready(function(){
+    update_amounts();
+    $('.qty, .price').on('keyup keypress blur change', function(e){
+        update_amounts();
+    });
+});
+
+function update_amounts(){
+    var sum = 0.0;
+    $('#cart_table > tbody > tr').each(function(){
+        var qty = $(this).find('.qty').val();
+        var price = $(this).find('.price').val();
+        var amount = (qty*price);
+        sum += amount;
+        $(this).find('.amount').text('' + amount);
+    });
+    $('.total').text(sum);
+}
+
+var incrementQty;
+var decrementQty;
+var plusBtn = $(".cart_qty_plus");
+var minusBtn = $(".cart_qty_minus");
+var incrementQty = plusBtn.click(function(){
+    var $n = $(this)
+    .parent(".btn_container")
+    .find(".qty");
+    $n.val(Number($n.val())+1);
+    update_amounts();
+});
+var decrementQty = minusBtn.click(function(){
+    var $n = $(this)
+    .parent(".btn_container")
+    .find(".qty");
+    var QtyVal = Number($n.val());
+    if (QtyVal > 0){
+        $n.val(QtyVal - 1);
+    }
+    update_amounts();
+});
